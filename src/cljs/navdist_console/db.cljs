313b26e9939688fee58c749b5b3f455e0fe77abe
marchrock <m4rchrock@gmail.com>
2018-08-24 22:25:52 +0900

    fixed db store

--- src/cljs/navdist_console/db.cljs
+++ src/cljs/navdist_console/db.cljs
@@ -2,13 +2,13 @@
 
 (defonce os (js/require "os"))
 
-;;: uri "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/"
+;;:uri "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/"
 ;; default db structure
 (def default-db
   {:config
    {:title "Navdist Console"
     :locale :ja
-    :uri "https://www.youtube.com/"
+    :uri "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/"
     :user-css ".dmm-ntgnavi,.area-naviapp,.area-pickupgame,.area-menu,#ntg-recommend,#foot{display:none!important}body{margin:0;padding:0;overflow:hidden}#game_frame{position:fixed;left:50%;top:-16px;margin-left:-600px;z-index:1}.area-pickupgame,.area-menu{display:none!important}"
     :screenshot {:path (str (.homedir os) "/Downloads/")
                  :prefix "navdist-"
