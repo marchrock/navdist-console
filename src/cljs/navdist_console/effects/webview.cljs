5f37b2c9e6ccba2c54a549193f4f8708027c4f26
marchrock <m4rchrock@gmail.com>
2018-09-05 07:15:15 +0900

    refactored debugger handler

--- src/cljs/navdist_console/effects/webview.cljs
+++ src/cljs/navdist_console/effects/webview.cljs
@@ -1,8 +1,10 @@
 (ns navdist-console.effects.webview
   (:require [re-frame.core :as re-frame]
             [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
+            [navdist-console.records :refer [ReceivedApi]]
             [taoensso.timbre :as timbre]))
 
+;; inject css to webview
 (defn-traced webview-injectcss-handler
   [req]
   (let [webview (:target-webview req)
@@ -39,37 +41,49 @@
 ;; inject debugger
 (def api-endpoint-match #"^http://.*/kcsapi/.+$")
 
-(defrecord ApiRecord [request-id response url headers])
-
 ;; queue for api response
 (def api-queue (atom ()))
 
-(defn debugger-message-handler
-  [debugger e m p]
-  (if (= m "Network.responseReceived")
-    (when (and (= (-> p .-response .-mimeType) "text/plain")
-               (re-find #"^http://.*/kcsapi/.+$" (-> p .-response .-url)))
+(defn network-response-received-filter
+  [req]
+  (when (= (:method req) "Network.responseReceived")
+    (when (and (= (-> (:params req) .-response .-mimeType) "text/plain")
+               (re-find api-endpoint-match (-> (:params req) .-response .-url)))
       (timbre/debug "responseReceived")
-      (let [headers (-> p .-response .-headers
-                        (js->clj :keywordize-keys true))]
-        (swap! api-queue conj (ApiRecord. (-> p .-requestId)
-                                          (-> p .-response)
-                                          (-> p .-response .-url)
-                                          headers)))))
-  (if (= m "Network.loadingFinished")
-    (let [request-id (-> p .-requestId)
+      (let [p (:params req)
+            headers (-> p .-response .-headers (js->clj :keywordize-keys true))]
+        (swap! api-queue conj (ReceivedApi. (-> p .-requestId)
+                                            (-> p .-response)
+                                            (-> p .-response .-url)
+                                            headers)))))
+  req)
+
+(defn network-loading-finished-filter
+  [req]
+  (when (= (:method req) "Network.loadingFinished")
+    (let [request-id (-> (:params req) .-requestId)
           record (-> (filter #(= request-id (:request-id %)) @api-queue)
                      first)]
       (when record
         (timbre/debug "loadingFinished")
-        (.sendCommand debugger "Network.getResponseBody"
+        (.sendCommand (:debugger req) "Network.getResponseBody"
                       (clj->js {:requestId request-id})
                       (fn [e r]
                         (timbre/spy (:url record))
                         (timbre/spy (:headers record))
                         (timbre/spy (.-body r))))
-        (swap! api-queue (fn [x] (remove #(= request-id (:request-id %)) x)))
-        (timbre/spy @api-queue)))))
+        (swap! api-queue (fn [x] (remove #(= request-id (:request-id %)) x))))))
+  req)
+
+(defn debugger-message-handler
+  [debugger e m p]
+  (let [req {:debugger debugger
+             :event e
+             :method m
+             :params p}]
+    (-> req
+        network-response-received-filter
+        network-loading-finished-filter)))
 
 (defn-traced inject-debugger
   [req]
