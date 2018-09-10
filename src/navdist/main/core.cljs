28fe6435841664f0a74cf9a86fa66e6b9832a5c0
marchrock <m4rchrock@gmail.com>
2018-09-10 22:23:26 +0900

    added new main for electron written in cljs

---
+++ src/navdist/main/core.cljs
@@ -0,0 +1,24 @@
+(ns navdist.main.core
+  (:require ["electron" :refer [app BrowserWindow]]))
+
+(def main-window (atom nil))
+
+(def app-name "Navdist Console")
+
+(defn init-electron
+  []
+  (reset! main-window (BrowserWindow.
+                       (clj->js {:width 1200
+                                 :height 768
+                                 :useContentsSize true
+                                 :frame false
+                                 :title app-name})))
+
+  (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
+
+  (.on @main-window "closed" #(reset! main-window nil)))
+
+(defn main
+  []
+  (.on app "window-all-closed" #(.quit app))
+  (.on app "ready" init-electron))
