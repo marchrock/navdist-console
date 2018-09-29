4278b23cfa02fadc099dc80d4ce087fa7eb48198
marchrock <m4rchrock@gmail.com>
2018-09-29 19:03:33 +0900

    fixed typo

--- src/navdist/app/cofx/core.cljs
+++ src/navdist/app/cofx/core.cljs
@@ -11,7 +11,7 @@
    (let [app (-> remote .-app)]
      (assoc cofx :os-home-dir (.getPath app "home")))))
 
-(re-fram/reg-cofx
+(re-frame/reg-cofx
  :user-data-dir
  (fn [cofx]
    (let [app (-> remote .-app)]
